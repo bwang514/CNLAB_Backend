c9a733cc39968b269d636a187de037d18157946e
haoen0514 <haoen0514@learnler.csie.ntu.edu.tw>
2017-06-12 22:54:23 +0800

    abc

--- mysite/clean_user.py
+++
@@ -1,6 +0,0 @@
-def getUserInformation(user_id):
-    conn = sqlite3.connect('Database.db3')
-    conn.execute("DELETE FROM User")
-    conn.close()
-    
-    return
--- mysite/trips/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("<p>Hello World!</p>")
-# Create your views here. 
